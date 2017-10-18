eb35286002232bf3c50b966e929284e05b969814
Anna Ivanenko <anna.ivanenko27@gmail.com>
2017-10-18 20:07:52 +0300

    Task done.

---
+++ src/ua/nure/ivanenko/Practice3/Demo.java
@@ -0,0 +1,20 @@
+package ua.nure.ivanenko.Practice3;
+
+import java.security.NoSuchAlgorithmException;
+
+public class Demo {
+	static final String[] ARG = {""};
+
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		System.out.println("=====Part1\n");
+		Part1.main(ARG);
+		System.out.println("\n=====Part2\n");
+		Part2.main(ARG);
+		System.out.println("\n=====Part3\n");
+		Part3.main(ARG);
+		System.out.println("\n=====Part4\n");
+		Part4.main(ARG);
+		System.out.println("\n=====Part5");
+		Part5.main(ARG);
+	}
+}
